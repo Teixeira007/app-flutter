54abed3d70bb39f023e2f85aeb37a663b096d41a
teixeira007 <viniciusteixeiravini16@gmail.com>
2025-01-03 15:23:04 -0300

    configurando thema

--- lib/components/main/main.dart
+++ lib/components/main/main.dart
@@ -1,5 +1,7 @@
 import 'package:app_academia/models/auth.dart';
-import 'package:app_academia/pages/auth_page.dart';
+import 'package:app_academia/pages/add_gym_page.dart';
+import 'package:app_academia/pages/auth_or_homepage.dart';
+import 'package:app_academia/providers/great_gym.dart';
 import 'package:app_academia/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,14 +19,34 @@ class _AcademiaApp extends State<AcademiaApp> {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => Auth())
+        ChangeNotifierProvider(create: (_) => Auth()),
+        ChangeNotifierProvider(create: (_) => GreatGym())
       ],
       child: MaterialApp(
-        initialRoute: AppRoutes.AUTH,
-        routes: {
-          AppRoutes.AUTH:  (context) => AuthPage()
-        },
+        theme: ThemeData(
+          primaryColor: Color(0xff1570EF),
+          secondaryHeaderColor: Color(0xff999999),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Color(0xff1570EF),
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.white, // Cor da seta de voltar
+          ),
+          // primaryColor: Color(0xff1570EF),
         ),
+        initialRoute: AppRoutes.AUTH_OR_HOME,
+        routes: {
+          AppRoutes.AUTH_OR_HOME: (context) => AuthOrHomepage(),
+          AppRoutes.ADD_GYM: (context) => AddGymPage(),
+        },
+      ),
     );
   }
 }
